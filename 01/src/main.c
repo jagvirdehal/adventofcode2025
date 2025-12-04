e2f48cb3092c7e36710a37c8629b459abc08ae36
Jagvir Dehal <jagvird216@gmail.com>
2025-12-03 21:36:38 -0800

    Finish 01.1

--- 01/src/main.c
+++ 01/src/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
+typedef enum : char {
+	LEFT = 'L',
+	RIGHT = 'R',
+} Dir;
+
 int main() {
-	printf("Test\n");
+	Dir d;
+	int amt;
+
+	int cumsum = 50;
+	int password = 0;
+	while (scanf("%c%d\n", &d, &amt) != EOF) {
+		switch (d) {
+			case LEFT:
+				cumsum -= amt;
+				break;
+			case RIGHT:
+				cumsum += amt;
+				break;
+		}
+		cumsum = (cumsum + 100) % 100;
+		if (cumsum == 0) {
+			password += 1;
+		}
+	}
+	printf("%d\n", password);
 	return 0;
 }
