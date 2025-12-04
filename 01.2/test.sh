1dfb8ad2525cfa7a78d5c057ed3d203c969f7638
Jagvir Dehal <jagvird216@gmail.com>
2025-12-03 22:35:15 -0800

    Move test-script to root

--- 01.2/test.sh
+++ 01.2/test.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-for i in $(fd '\.in$'); do
-	TESTNAME="$(dirname $i)/$(basename -s.in $i)"
-	DIFFOUT=$(diff <(./exec <${TESTNAME}.in) ${TESTNAME}.out)
-	if [ $? != 0 ]; then
-		echo "=== $TESTNAME FAILED ==="
-		echo "$DIFFOUT"
-		echo
-	fi
-done
+../test-script
